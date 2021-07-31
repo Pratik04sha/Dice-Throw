b25b27fd94a1763022d42760aadedfa5242b1220
Pratiksha Patil <67996947+Pratik04sha@users.noreply.github.com>
2021-07-31 20:28:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+// randomNumber1= Math.floor(randomNumber1)+1;
+// document.querySelector("img1").setAttribute()
+
+
+var randomNumberDice= "dice"+randomNumber1+".png";
+var randomImageSource= "images/"+ randomNumberDice;
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomNumberDice2= "dice"+randomNumber2+".png";
+var randomImageSource2="images/"+randomNumberDice2;
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1> randomNumber2) {
+    document.querySelector("h1").innerHTML="🥳Dice1 Won!🥳";
+}
+else if(randomNumber2> randomNumber1){
+    document.querySelector("h1").innerHTML="🥳Dice2 Won!🥳";
+}
+else{
+    document.querySelector("h1").innerHTML="🤩It's a Tie!🤩";
+}
